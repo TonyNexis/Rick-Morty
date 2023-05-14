86a7eb0197a14fe8975fb9ea433e4c1361ca0286
Anton <tony.zenekis@gmail.com>
2023-05-14 22:41:22 +0300

    class testing v0

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 import filter from './modules/filter.js';
 import profile from './modules/profile.js';
 import getCharacters from './modules/getCharacters.js';
+import markupGenerator from './modules/markupGenerator.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 getCharacters();
 filter();
 profile();
+markupGenerator();
 });
