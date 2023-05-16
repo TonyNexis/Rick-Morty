4a4b001cc245c05fc3c45a71caab7ed0d7169415
Anton <tony.zenekis@gmail.com>
2023-05-17 00:32:01 +0300

    mainPage

--- src/js/modules/ElementCreator.js
+++ src/js/modules/ElementCreator.js
@@ -24,6 +24,13 @@ class ElementCreator {
         return this;
     }
 
+    setInnerHtml(html) {
+        if (this.element && html) {
+            this.element.innerHTML = html;
+        }
+        return this;
+    }
+
     setTextContent(text) {
         if (this.element && text) {
             this.element.textContent = text;
@@ -38,30 +45,5 @@ class ElementCreator {
         return this;
     }
 }
-
-    const build = new ElementCreator();
-
-    const mainPage = build.create('div')
-        .addClass('flex-container')
-        .addClass('main_page')
-        .appendTo(document.body);
-
-    const header = build.create('header')
-        .appendTo(document.querySelector('.main_page'));
-
-    const mainImg = build.create('img')
-        .addClass('main_img')
-        .setAttribute({ src: './assets/img/PngItem_438051.png', alt: 'Rick & Morty' })
-        .appendTo(document.querySelector('header'));
-
-        // searchBar
-
-    const cards = build.create('div')
-        .addClass('cards')
-        .appendTo(document.querySelector('.main_page'));
-
-    const profilePage = build.create('div')
-        .addClass('profile_page')
-        .appendTo(document.body);
 
 export default ElementCreator;
--- src/js/modules/profile.js
+++ src/js/modules/profile.js
@@ -1,11 +1,36 @@
 import DataFetcher from './DataFetcher.js';
+import ElementCreator from './ElementCreator.js';
 
 const profile = () => {
-    const main = document.querySelector('.main_page'),
+      const build = new ElementCreator();
+
+
+      const profilePage = build.create('div')
+          .addClass('profile_page')
+          .appendTo(document.body);
+
+      const profilePageBtn = build.create('button')
+          .addClass('profile_page_btn')
+          .appendTo(document.querySelector('.profile_page'));
+
+      const arrowWrapper = build.create('span')
+          .setAttribute({ id: 'arrowSpan' })
+          .appendTo(document.querySelector('.profile_page_btn'));
+
+      const arrow = build.create('svg')
+          .addClass('arrow')
+          .setAttribute({ width: 16, height: 16, viewBox: '0 0 16 16' })
+          .setInnerHtml('<path d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z"/>')
+          .appendTo(document.getElementById('arrowSpan'));
+
+      const goHome = build.create('span')
+          .setTextContent('GO HOME')
+          .appendTo(document.querySelector('.profile_page_btn'));
+
+      const main = document.querySelector('.main_page'),
           goBackBtn = document.querySelector('.profile_page_btn'),
-          profilePage = document.querySelector('.profile_page'),
           cards = document.querySelector('.cards'),
-          profileBlock = document.querySelector('.profile');
+          test = document.querySelector('.profile_page');
 
     cards.addEventListener('click', (e) => {
         const card = e.target.closest('.card');
@@ -31,38 +56,41 @@ const profile = () => {
                     statusIcon = 'status_icon_unknown';
                 }
 
-                profileBlock.innerHTML = `
-            <img class="profile_img" src="${character.image}" alt="profile_image">
-            <p class="profile_name">${character.name}</p>
-            <p class="profile_info">Informations</p>
-            <div class="profile_container">
-              <p class="profile_cont_mainText">Gender</p>
-              <p class="profile_cont_secondText">${character.gender}</p>
-            </div>
-            <div class="profile_container">
-              <p class="profile_cont_mainText">Status</p>
-              <p class="profile_cont_secondText"><span class="${statusIcon}"></span>${character.status}</p>
-            </div>
-            <div class="profile_container">
-              <p class="profile_cont_mainText">Specie</p>
-              <p id="race" class="profile_cont_secondText">${character.species}</p>
-            </div>
-            <div class="profile_container">
-              <p class="profile_cont_mainText">Origin</p>
-              <p class="profile_cont_secondText">${character.origin.name}</p>
-            </div>
-            <div class="profile_container">
-              <p class="profile_cont_mainText">Type</p>
-              <p class="profile_cont_secondText">${type}</p>
-            </div>
-            <div class="profile_container">
-            <p class="profile_cont_mainText">Episodes</p>
-            <p class="profile_cont_secondText">${character.episode.length}</p>
-          </div>`;
+
+
+
+          //       profileBlock.innerHTML = `
+          //   <img class="profile_img" src="${character.image}" alt="profile_image">
+          //   <p class="profile_name">${character.name}</p>
+          //   <p class="profile_info">Informations</p>
+          //   <div class="profile_container">
+          //     <p class="profile_cont_mainText">Gender</p>
+          //     <p class="profile_cont_secondText">${character.gender}</p>
+          //   </div>
+          //   <div class="profile_container">
+          //     <p class="profile_cont_mainText">Status</p>
+          //     <p class="profile_cont_secondText"><span class="${statusIcon}"></span>${character.status}</p>
+          //   </div>
+          //   <div class="profile_container">
+          //     <p class="profile_cont_mainText">Specie</p>
+          //     <p id="race" class="profile_cont_secondText">${character.species}</p>
+          //   </div>
+          //   <div class="profile_container">
+          //     <p class="profile_cont_mainText">Origin</p>
+          //     <p class="profile_cont_secondText">${character.origin.name}</p>
+          //   </div>
+          //   <div class="profile_container">
+          //     <p class="profile_cont_mainText">Type</p>
+          //     <p class="profile_cont_secondText">${type}</p>
+          //   </div>
+          //   <div class="profile_container">
+          //   <p class="profile_cont_mainText">Episodes</p>
+          //   <p class="profile_cont_secondText">${character.episode.length}</p>
+          // </div>`;
             });
 
         main.style.display = 'none';
-        profilePage.style.display = 'block';
+        test.style.display = 'block';
         }
     });
 
