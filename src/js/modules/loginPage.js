9ef0c0c7e4bda2331af0ac2ad9361e4daf00a2ff
Anton <tony.zenekis@gmail.com>
2023-05-18 23:28:53 +0300

    loginPage v0.2

--- src/js/modules/loginPage.js
+++ src/js/modules/loginPage.js
@@ -14,11 +14,64 @@ const loginPage = () => {
         .appendTo(document.querySelector('.login_page'));
 
     const helloText = build.create('p')
+        .addClass('login_header')
         .setTextContent('Hello! You need to login')
         .appendTo(document.querySelector('.login_form'));
 
     const form = build.create('form')
-        .appendTo(document.querySelector('login_form'));
+        .addClass('loginFormWrapper')
+        .appendTo(document.querySelector('.login_form'));
+
+    const loginText = build.create('p')
+        .addClass('loginText')
+        .setTextContent('Login:')
+        .appendTo(document.querySelector('form'));
+
+    const loginBlock = build.create('div')
+        .addClass('inputBlock')
+        .setAttribute({ id: 'loginBlock' })
+        .appendTo(document.querySelector('form'));
+
+    const loginInput = build.create('input')
+        .addClass('inputWindow')
+        .setAttribute({ placeholder: 'Your login...', type: 'text', name: 'login' })
+        .appendTo(document.querySelector('#loginBlock'));
+
+    const passwordText = build.create('p')
+        .addClass('loginText')
+        .setTextContent('Password:')
+        .appendTo(document.querySelector('form'));
+
+    const passwordBlock = build.create('div')
+        .addClass('inputBlock')
+        .setAttribute({ id: 'passwordBlock' })
+        .appendTo(document.querySelector('form'));
+
+    const passwordInput = build.create('input')
+        .addClass('inputWindow')
+        .setAttribute({ placeholder: 'Your password...', type: 'password', name: 'password' })
+        .appendTo(document.querySelector('#passwordBlock'));
+
+    const btnForm = build.create('button')
+        .addClass('btnForm')
+        .setTextContent('Lets start')
+        .appendTo(document.querySelector('form'));
+
+    const registrationUrl = build.create('a')
+        .setAttribute({ id: 'regUrl' })
+        .setTextContent('Registration')
+        .appendTo(document.querySelector('.login_form'));
+
+    const buttonLogin = document.querySelector('.btnForm'),
+          loginFormWrapWrap = document.querySelector('.loginFormWrapper');
+
+    loginFormWrapWrap.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const loginFormData = new FormData(loginFormWrapWrap);
+
+        console.log(loginFormData);
+    });
 };
 
 export default loginPage;
