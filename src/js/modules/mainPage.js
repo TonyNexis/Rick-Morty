99fb2b2dc6c219270a83362989a3fee1eee36c3f
TonyNexis <102922739+TonyNexis@users.noreply.github.com>
2023-05-28 23:07:39 +0300

    Revert "class structure app"

--- src/js/modules/mainPage.js
+++ src/js/modules/mainPage.js
@@ -1,83 +1,66 @@
-import ElementCreator from '../services/ElementCreator.js';
-import ProfileFilter from '../services/ProfileFilter.js';
+import ElementCreator from './ElementCreator.js';
 
-export default class MainPage {
-    constructor() {
-        this.build = new ElementCreator();
-        this.profileFilter = new ProfileFilter();
-    }
+const mainPage = () => {
+    const build = new ElementCreator();
 
-    createPage() {
-        this.mainPageWrapper = this.build.create('div')
+    const mainPageWrapper = build.create('div')
         .addClass('flex-container')
         .addClass('main_page')
         .addClass('hide')
         .appendTo(document.body);
 
-        this.userBlock = this.build.create('div')
-            .setAttribute({ id: 'userBlock' })
-            .appendTo(document.querySelector('.main_page'));
+    const login = localStorage.getItem('login'),
+          userDataObj = JSON.parse(localStorage.getItem('userData'));
 
-        this.user = this.build.create('span')
-            .addClass('user')
-            .appendTo(document.querySelector('#userBlock'));
+    const userBlock = build.create('div')
+         .setAttribute({ id: 'userBlock' })
+         .appendTo(document.querySelector('.main_page'));
 
-        this.login = localStorage.getItem('login');
-            if (this.login) {
-                this.userDataObj = JSON.parse(localStorage.getItem('userData'));
-                this.user.setTextContent(`Hello, ${this.userDataObj.login}`);
-            }
+    const user = build.create('span')
+        .addClass('user')
+        .setTextContent(`Hello, ${userDataObj.login}`)
+        .appendTo(document.querySelector('#userBlock'));
 
-        this.logoutUrl = this.build.create('a')
-            .addClass('user')
-            .setAttribute({ id: 'logoutUrl', href: '' })
-            .setTextContent('(Logout)')
-            .appendTo(document.querySelector('#userBlock'));
+    const logoutUrl = build.create('a')
+        .addClass('user')
+        .setAttribute({ id: 'logoutUrl', href: '' })
+        .setTextContent('(Logout)')
+        .appendTo(document.querySelector('#userBlock'));
 
-        this.header = this.build.create('header')
-            .appendTo(document.querySelector('.main_page'));
+    const header = build.create('header')
+        .appendTo(document.querySelector('.main_page'));
 
-        this.mainImg = this.build.create('img')
-            .addClass('main_img')
-            .setAttribute({ src: './assets/img/PngItem_438051.png', alt: 'Rick & Morty' })
-            .appendTo(document.querySelector('header'));
+    const mainImg = build.create('img')
+        .addClass('main_img')
+        .setAttribute({ src: './assets/img/PngItem_438051.png', alt: 'Rick & Morty' })
+        .appendTo(document.querySelector('header'));
 
-        this.searchBar = this.build.create('div')
-            .addClass('search_bar')
-            .appendTo(document.querySelector('.main_page'));
+    const searchBar = build.create('div')
+        .addClass('search_bar')
+        .appendTo(document.querySelector('.main_page'));
 
-        this.btnSearch = this.build.create('button')
-            .addClass('btn_search')
-            .setAttribute({ type: 'submit' })
-            .appendTo(document.querySelector('.search_bar'));
+    const btnSearch = build.create('button')
+        .addClass('btn_search')
+        .setAttribute({ type: 'submit' })
+        .appendTo(document.querySelector('.search_bar'));
 
-        this.searchImg = this.build.create('img')
-            .addClass('search_img')
-            .setAttribute({ src: './assets/img/Vector.svg', alt: 'search' })
-            .appendTo(document.querySelector('.btn_search'));
+    const searchImg = build.create('img')
+        .addClass('search_img')
+        .setAttribute({ src: './assets/img/Vector.svg', alt: 'search' })
+        .appendTo(document.querySelector('.btn_search'));
 
-        this.searchInput = this.build.create('input')
-            .addClass('search_input')
-            .setAttribute({
-            id: 'name-filter', placeholder: 'Filter by name...', type: 'text', name: 'filter',
-            })
-            .appendTo(document.querySelector('.search_bar'));
+    const searchInput = build.create('input')
+        .addClass('search_input')
+        .setAttribute({ id: 'name-filter', placeholder: 'Filter by name...', type: 'text', name: 'filter' })
+        .appendTo(document.querySelector('.search_bar'));
 
-        this.cards = this.build.create('div')
-            .addClass('cards')
-            .appendTo(document.querySelector('.main_page'));
+    const cards = build.create('div')
+        .addClass('cards')
+        .appendTo(document.querySelector('.main_page'));
 
-        return this;
-    }
-
-    initEventListeners() {
-        this.logoutUrl = document.querySelector('#logoutUrl').addEventListener('click', (e) => {
+        document.querySelector('#logoutUrl').addEventListener('click', (e) => {
             localStorage.setItem('login', false);
         });
-    }
+};
 
-    filter() {
-        this.ProfileFilter = this.profileFilter()
-            .search();
-    }
-}
+export default mainPage;
